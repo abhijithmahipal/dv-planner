ca24f5877858e0cd7e28d91a829031d3c0a20e5b
Abhijith Mahipal <abhijithm@valorem.com>
2020-07-05 21:00:21 +0530

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyAxg5n6iQy9S9jgO1xTLGDB63xQ1QDwgic',
+    authDomain: 'dv-planner-app.firebaseapp.com',
+    databaseURL: 'https://dv-planner-app.firebaseio.com',
+    projectId: 'dv-planner-app',
+    storageBucket: 'dv-planner-app.appspot.com',
+    messagingSenderId: '905129378478',
+    appId: '1:905129378478:web:845bc47ce9ed39a9dfe33f'
+  }
 };
 
 /*
